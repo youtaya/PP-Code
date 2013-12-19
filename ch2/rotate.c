a639c0d52c416cbddb579d97e07e4a00e45ce389
youtaya <jxiaoping@gmail.com>
2013-12-19 23:58:44 +0800

    add different algorithm

--- ch2/rotate.c
+++ ch2/rotate.c
@@ -1,6 +1,19 @@
+/*
+ * rotate.c
+ *
+ *  Created on: 2013-12-18
+ *      Author: youtaya
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
+
+typedef enum {
+	forward,
+	backward,
+}direct;
 
 void rotate(char *str, int n, int i)
 {
@@ -39,14 +52,69 @@ void rotate(char *str, int n, int i)
 
 }
 
+void swap(char *str,int waitLen, int left, int right, int start, int range, int stop, int dir)
+{
+	int i;
+	char temp;
+
+	if(1 == waitLen||0 == left||0 == right)
+		return;
+
+	for(i=0;i<range;i++)
+	{
+		temp = str[start+i];
+		str[start+i] = str[stop-range+1+i];
+		str[stop-range+1+i] = temp;
+	}
+
+	waitLen = waitLen-range;
+
+	switch(dir)
+	{
+	case backward:
+		left = range;
+		right = waitLen-range;
+		stop = stop-range;
+		break;
+	case forward:
+		left = waitLen-range;
+		right = range;
+		start = start+range;
+		break;
+	}
+
+	if(right > left)
+	{
+		dir = backward;
+		range = left;
+	}else {
+		dir = forward;
+		range = right;
+	}
+
+	printf("params: %d, %d, %d, %d, %d, %d, %d\n", waitLen, left, right, start, range, stop, dir);
+	swap(str, waitLen, left, right, start, range, stop, dir);
+}
 void rotate2(char *str, int n, int i)
 {
+	int left = i;
+	int right = n-i;
+	int stop = n-1;
+	int start, range;
+	direct dir;
 
+	assert(right > left);
+
+	start = 0;
+	range = left;
+	dir = backward;
+
+	swap(str, n, left, right, start, range, stop, dir);
 }
 
 void reverse(char *str, int start, int stop)
 {
-    int i,j;
+    int j;
     int len = stop-start;
     int range = len/2;
     char temp;
@@ -71,17 +139,15 @@ int main(int argc, char* argv[])
     int len;
     char a[]={'a','b','c','d','e','f','g'};
     char b[]={'a','b','c','d','e','f','g','h','i'};
-    //not support this case:
-    //char c[]={'a','b','c','d','e'};
 
     len = sizeof(a);
-    rotate3(a, len, 3);
+    rotate2(a, len, 3);
     for(i=0;i<len;i++)
 	    printf("%c ",a[i]);
     printf("\n");
 
     len = sizeof(b);
-    rotate3(b, len, 3);
+    rotate2(b, len, 3);
     for(i=0;i<len;i++)
 	    printf("%c ",b[i]);
     printf("\n");
